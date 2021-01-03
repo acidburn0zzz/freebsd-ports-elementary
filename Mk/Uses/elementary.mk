dd22fb2b29fefac5e29b65085a9e36a86759b345
Olivier Duchateau <duchateau.olivier@gmail.com>
2021-01-03 19:44:18 +0100

    Fix SWITCHBOARD_PLUGDIR macro

--- Mk/Uses/elementary.mk
+++ Mk/Uses/elementary.mk
@@ -20,7 +20,7 @@ IGNORE= Incorrect USES+= elementary:${elementary_ARGS} takes no arguments
 
 DIST_SUBDIR=	elementary
 
-PLIST_SUB+=	SWITCHBOARD_PLUGDIR="lib/switchboard/system"
+PLIST_SUB+=	SWITCHBOARD_PLUGDIR="lib/switchboard"
 
 # Available Elementary components are:
 _USE_ELEMENTARY_ALL=  gala granite plank switchboard wingpanel
